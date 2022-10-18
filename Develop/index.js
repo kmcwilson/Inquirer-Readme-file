481babb751c9083b384a42874260168a546ff229
Katie Wilson <kmcwilson4@gmail.com>
2022-10-18 15:49:37 -0400

    added a code to build the README file and am adding the sections to that file

--- Develop/index.js
+++ Develop/index.js
@@ -1,73 +1,85 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-
+const fs = require('fs');
+const generateMarkdown= require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your gitHub username?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?',
-        },
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?'
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your gitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
 
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Write a short description of your project',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What kind of license does your project need?',
-            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-        },
-        {
-            type: 'input',
-            name: 'dependencies',
-            message: 'What command should be run to install dependencies?',
-            default: 'npm i',
-        }, 
-        {
-            type: 'input',
-            name: 'tests', 
-            message: 'What command should be run to run tests?',
-            default: 'npm test',
-        },
-        {
-            type: 'input',
-            name: 'user-info', 
-            message: 'What does the user need to know about using this repo?',
-        },
-        { 
-            type: 'input',
-            name: 'contribution',
-            message: 'What does the user need to know about contributing to this repo?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description of your project',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license does your project need?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        name: 'user-info',
+        message: 'What does the user need to know about using this repo?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What does the user need to know about contributing to this repo?'
 
 
-        }
-           
+    }
 
-        ]
-
-    )
-const questions = [];
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+
+    fs.writeFile(`./assets/${fileName}`, data, (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
+
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            console.log(data);
+            const fileName = `${data.title.split(' ').join('-')}.md`;
+            const markdownContent= generateMarkdown(data);
+            writeToFile(fileName, markdownContent);
+        }
+        )
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  `(https://img.shields.io/badge/license-${data.license}MIT-green)`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const badge = renderLicenseBadge(data.license);
+  const link = renderLicenseLink(data.license);
+  const section = renderLicenseSection(data.license);
   return `# ${data.title}
+${badge}
+
+  ## Table of Contents 
+
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+
+
+  ## Description
+${data.description}
+
+  ## License
+  ${section}
+  ${badge}
+  ${link}
+
+  ##Questions
+  [${data.github}](https://github.com/${data.github})
+
+  Any further questions can be sent to:
+
+  [${data.email}](mailto:${data.email})
+  
+
+
 
 `;
+
 }
 
 module.exports = generateMarkdown;
